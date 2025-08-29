f2ff597389675c52ce41bd017ff53803eb4f56b8
Sabari-work <sabaris.officialwork@gmail.com>
2025-08-30 02:14:48 +0530

    arguments studied

--- arguments.java
+++ arguments.java
@@ -1,3 +1,13 @@
 class arguments {
-    public static void main
+    public static void main(String args[])
+    {
+        arguments obj= new arguments();
+        int a=10,b=10;
+        obj.sum(a,b);
+    }
+    
+    void sum(int c, int d)
+    {
+       System.out.println(c+d); 
+    }
 }
